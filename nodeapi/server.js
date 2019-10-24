59c35a253290e7247054d20908896bb488249afc
carloshrf <carlos_6282010@hotmail.com>
2019-10-24 13:55:40 -0300

    Adicionada pasta do api inicial NodeJS

---
+++ nodeapi/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//============================================================
+
+//Iniciar app
+const app = express();
+//Permitir que a api receba dados em formato json
+app.use(express.json());
+//Utiliza o CORS na aplicação
+app.use(cors());
+
+//============================================================
+
+//Conexão com o banco de dados
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser: true});
+
+//Expressão require comum =>       require('./src/models/Product');
+requireDir('./src/models');
+
+//Ao definir "/api" todas requisições serão chamadas iniciando por /api/
+//app é para receber qualquer tipo de requisição
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
