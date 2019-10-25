9fdaa35ad26e50421337c18d981052d31cdf1b6b
carloshrf <carlos_6282010@hotmail.com>
2019-10-25 16:52:38 -0300

    Comentários adicionais

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -9,7 +9,7 @@ const requireDir = require('require-dir');
 const app = express();
 //Permitir que a api receba dados em formato json
 app.use(express.json());
-//Utiliza o CORS na aplicação
+//Utiliza o CORS na aplicação (Responsável por permitir acesso externo à API)
 app.use(cors());
 
 //============================================================
@@ -18,7 +18,7 @@ app.use(cors());
 mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser: true});
 
 //Expressão require comum =>       require('./src/models/Product');
-requireDir('./src/models');
+requireDir('./src/models'); //Permite agora que seja executado o require em todos aplicativos automaticamente
 
 //Ao definir "/api" todas requisições serão chamadas iniciando por /api/
 //app é para receber qualquer tipo de requisição
